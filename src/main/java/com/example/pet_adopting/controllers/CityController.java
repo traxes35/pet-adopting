0267c522c2096eefcf3730f89b916e31bb82f22b
traxes35 <traxes121@gmail.com>
2024-11-30 21:33:22 +0300

    Initial commit

--- src/main/java/com/example/pet_adopting/controllers/CityController.java
+++ src/main/java/com/example/pet_adopting/controllers/CityController.java
@@ -2,6 +2,8 @@ package com.example.pet_adopting.controllers;
 
 import com.example.pet_adopting.entities.City;
 import com.example.pet_adopting.services.CityService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +18,32 @@ CityService cityService;
     }
 
     @GetMapping
-    public List<City> getAllCities() {
-        return cityService.getAllCities();
+    public ResponseEntity<?> getAllCities() { // ResponseEntity<?> tipinde döndürme
+        List<City> cities = cityService.getAllCities();
+
+        if (cities.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("There is no added city");
+        }
+        return ResponseEntity.ok(cities); // Başarılı durumda 200 OK ile liste döndür
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCityById(@PathVariable Long id) {
+        City city = cityService.getOneCitybyId(id);
+        if (city == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("City with ID " + id + " not found.");
+        }
+        return ResponseEntity.ok(city);
     }
     @PostMapping
-    public City createCity(@RequestBody City newCity) {
-        return cityService.saveOneCity(newCity);
-    }
-    @GetMapping("/{cityId}")
-    public City getOneCity(@PathVariable long cityId) {
-        return  cityService.getOneCitybyId(cityId);
-
+    public ResponseEntity<?> addCity(@RequestBody City city) {
+        if (city.getName() == null || city.getName().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("City name cannot be empty.");
+        }
+        City savedCity = cityService.saveOneCity(city);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCity);
     }
     @PutMapping("/{cityId}")
     public City updateOneCity(@PathVariable long cityId, @RequestBody City newCity) {
